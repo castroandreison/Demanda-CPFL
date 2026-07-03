fddf85b311f8cc3f465155cdf2c99de235f6a4bd
castroandreison <castroandreison@gmail.com>
2026-07-03 09:30:33 -0300

    Ajustes qm

--- Web_app_render/app.py
+++ Web_app_render/app.py
@@ -348,4 +348,4 @@ def api_coord_selet():
         return jsonify({'success': False, 'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
